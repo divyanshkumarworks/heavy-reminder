595095f91201857000884a2beb79dd1f40663274
rishu <divyanshkumarworks@gmail.com>
2023-06-09 18:33:15 +0530

    added login required feature

--- mainapp/views/template_view.py
+++ mainapp/views/template_view.py
@@ -2,20 +2,28 @@ from django.shortcuts import render, get_object_or_404
 from mainapp.models.task import Task
 from mainapp.models.task_user import TaskUser
 from datetime import datetime
+from django.http import HttpResponseRedirect
 
 import pytz
 
 def home(request):	
-	tz_kt = pytz.timezone('Asia/Kolkata')
-	current_date = datetime.now(tz_kt).strftime("%#d")
-	current_month = datetime.now(tz_kt).strftime("%b").upper()
 
-	context = {
-		"date": current_date,
-		"month": current_month
-	}
+	token = request.COOKIES.get('jwt_token')
 
-	return render(request,"mainapp/home.html", context)
+	if token:
+
+		tz_kt = pytz.timezone('Asia/Kolkata')
+		current_date = datetime.now(tz_kt).strftime("%#d")
+		current_month = datetime.now(tz_kt).strftime("%b").upper()
+
+		context = {
+			"date": current_date,
+			"month": current_month
+		}
+
+		return render(request,"mainapp/home.html", context)
+	else:
+		return HttpResponseRedirect('/login')
 
 def add_task(request):
 	return render(request, "mainapp/add_task.html")
@@ -24,11 +32,13 @@ def update_task(request, task_id):
 	obj = get_object_or_404(Task, id=task_id)
 	return render(request, "mainapp/update_task.html", {'obj': obj})
 
-def register(request):
-	return render(request, "mainapp/register.html")
-
 def profile(request):
-	return render(request, "mainapp/profile.html")
+	token = request.COOKIES.get('jwt_token')
+
+	if token:
+		return render(request, "mainapp/profile.html")
+	else:
+		return HttpResponseRedirect('/login')
 
 def login(request):
 	return render(request, "mainapp/login.html")
